12924beb64679b0a17b66f3ca96ae21f08f2a3b5
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2022-11-11 11:05:15 -0300

    Components
    Components

---
+++ 002. Components in React/src/components/script-body.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Corpo() {
+
+    return (
+
+        <section>
+
+            <h2>Canal com conteúdo de jogos</h2>
+            <p>Jogos de PS4</p>
+            <p>Jogos de outras plataformas</p>
+
+        </section>
+
+    )
+
+}
