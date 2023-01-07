523254a3bf91ab8039b6a3666fb50be4507d0973
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2023-01-07 16:44:49 -0300

    Routes Default
    Routes Default

---
+++ 011. Login Project/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import GlobalStyle from './global-style'
+import RouterApp from './Routes/routes-index'
+
+const App = () => {
+  return (
+    <>
+        Vasco <br/><br/>
+        < RouterApp />
+        < GlobalStyle />
+    </>
+  )
+}
+
+export default App
